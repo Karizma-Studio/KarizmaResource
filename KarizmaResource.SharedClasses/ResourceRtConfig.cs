7c2b07670e770a34ed49b5de714a944315a7204d
Mohammad Ghasemi <mohammad.ghasemi@karizmastudio.com>
2025-02-05 15:41:59 +0330

    add Rt config

---
+++ KarizmaResource.SharedClasses/ResourceRtConfig.cs
@@ -0,0 +1,17 @@
+﻿using KarizmaPlatform.Resources.SharedClasses.Enums;
+using KarizmaPlatform.Resources.SharedClasses.JsonSchemas;
+using Reinforced.Typings.Fluent;
+
+namespace KarizmaPlatform.Resources.SharedClasses
+{
+    public class ResourceRtConfig
+    {
+        public static void Configure(ConfigurationBuilder builder)
+        {
+            builder.ExportAsClass<ResourceChange>()
+                .WithPublicProperties();
+
+            builder.ExportAsEnum<ResourceType>();
+        }
+    }
+}
